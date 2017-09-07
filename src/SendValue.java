41af6b3ab57abb487e7d3b1720db918a5bfe8825
LoveMyDouDou <605988578@qq.com>
2017-09-07 11:19:55 +0800

    test

---
+++ src/SendValue.java
@@ -0,0 +1,12 @@
+
+public class SendValue {
+    public String str="6";
+    public static void main(String [] args){
+        SendValue sv=new SendValue();
+        sv.change(sv.str);
+        System.out.println(sv.str);
+    }
+    public void change(String str){
+        str="10";
+    }
+}
